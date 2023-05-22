baeba8a85b666545aa9d61da9366a32f561fbcb6
rafapaezbas <rpaezbas@rpaezbas.com>
2023-05-22 15:23:19 +0200

    *

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const Hyperswarm = require('hyperswarm')
 const ram = require('random-access-memory')
 
 const keys = JSON.parse(process.argv[2])
-console.log(keys)
+const range = false
 
 const main = async () => {
   const swarm = new Hyperswarm()
@@ -12,34 +12,66 @@ const main = async () => {
   const coreB = new Hypercore(ram, keys[1])
   const coreC = new Hypercore(ram, keys[2])
 
+  const coreE = new Hypercore(ram, keys[3])
+  const coreF = new Hypercore(ram, keys[4])
+  const coreG = new Hypercore(ram, keys[5])
+
   await coreA.ready()
   await coreB.ready()
   await coreC.ready()
+
+  await coreE.ready()
+  await coreF.ready()
+  await coreG.ready()
 
   swarm.join(coreA.discoveryKey)
   swarm.join(coreB.discoveryKey)
   swarm.join(coreC.discoveryKey)
 
-  swarm.on('connection', (conn) => {
+  swarm.join(coreE.discoveryKey)
+  swarm.join(coreF.discoveryKey)
+  swarm.join(coreG.discoveryKey)
+
+  swarm.on('connection', (conn, peer) => {
     coreA.replicate(conn)
     coreB.replicate(conn)
     coreC.replicate(conn)
+    coreE.replicate(conn)
+    coreF.replicate(conn)
+    coreG.replicate(conn)
   })
 
   await swarm.flush()
 
+  console.log('Range', range)
+
   console.log('1000 * 128')
-  await download(coreA, 1000)
+  await download(coreA, 1000, range)
   console.log('1000 * 512')
-  await download(coreB, 1000)
+  await download(coreB, 1000, range)
   console.log('1000 * 4096')
-  await download(coreC, 1000)
+  await download(coreC, 1000, range)
+
+  console.log('2000 * 128')
+  await download(coreE, 2000, range)
+  console.log('2000 * 512')
+  await download(coreF, 2000, range)
+  console.log('2000 * 4096')
+  await download(coreG, 2000, range)
+
+  process.exit()
 }
 
 const download = async (core, length) => {
   const start = Date.now()
-  for (let i = 0; i < length; i++) {
-    await core.get(i)
+  if (!range) {
+    for (let i = 0; i < length; i++) {
+      await core.get(i)
+    }
+  } else {
+    const range_ = []
+    for (let i = 0; i < length; i++) range_.push(i)
+    await (core.download(range_)).done()
   }
   const elapsed = Date.now() - start
   console.log('Elapsed', elapsed)
--- server.js
+++ server.js
@@ -20,19 +20,49 @@ const main = async () => {
   await coreC.ready()
   for (let i = 0; i < 1000; i++) await coreC.append(Buffer.alloc(4096))
 
+  // hypercore 2000 * 128
+  const coreE = new Hypercore(ram)
+  await coreE.ready()
+  for (let i = 0; i < 2000; i++) await coreE.append(Buffer.alloc(128))
+
+  // hypercore 2000 * 512
+  const coreF = new Hypercore(ram)
+  await coreF.ready()
+  for (let i = 0; i < 2000; i++) await coreF.append(Buffer.alloc(512))
+
+  // hypercore 2000 * 4096
+  const coreG = new Hypercore(ram)
+  await coreG.ready()
+  for (let i = 0; i < 2000; i++) await coreG.append(Buffer.alloc(4096))
+
   swarm.join(coreA.discoveryKey)
   swarm.join(coreB.discoveryKey)
   swarm.join(coreC.discoveryKey)
+  swarm.join(coreE.discoveryKey)
+  swarm.join(coreF.discoveryKey)
+  swarm.join(coreG.discoveryKey)
 
   swarm.on('connection', (conn) => {
     coreA.replicate(conn)
     coreB.replicate(conn)
     coreC.replicate(conn)
+    coreE.replicate(conn)
+    coreF.replicate(conn)
+    coreG.replicate(conn)
   })
 
   await swarm.flush()
 
-  console.log(JSON.stringify([coreA.key.toString('hex'), coreB.key.toString('hex'), coreC.key.toString('hex')]))
+  const keys = [
+    coreA.key.toString('hex'),
+    coreB.key.toString('hex'),
+    coreC.key.toString('hex'),
+    coreE.key.toString('hex'),
+    coreF.key.toString('hex'),
+    coreG.key.toString('hex')
+  ]
+
+  console.log(JSON.stringify(keys))
 }
 
 main()
